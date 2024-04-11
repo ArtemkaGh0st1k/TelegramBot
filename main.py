2c842d781db632752771c0a247220e26019e4b06
ArtemkaGh0st1k <artem4uk.6class.october@yandex.ru>
2024-04-11 15:48:01 +0400

    Add TOKEN to .env

---
+++ main.py
@@ -0,0 +1,11 @@
+import os
+import asyncio
+import logging
+
+from dotenv import load_dotenv
+from aiogram import Bot, Dispatcher
+
+
+load_dotenv()
+bot = Bot(token=os.getenv('TOKEN'), parse_mode='html')
+dp = Dispatcher()
