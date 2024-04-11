a212e74a9aa97e3064a396fb3597e9f42d67576a
ArtemkaGh0st1k <artem4uk.6class.october@yandex.ru>
2024-04-11 16:15:53 +0400

    Add params in 3 modules

---
+++ MainParameters/DeleteParams.py
@@ -0,0 +1,11 @@
+#FIXME: Подробно написать описание данного модуля
+'''
+DeleteParams.py
+
+Описание: Данный модуль предназначен 
+для команд удаление и только
+'''
+
+PICK_NAME_SURNAME: str | None = None
+PICK_OBJECT: str | None = None
+NameSurnameList: list[str] = []
---
+++ MainParameters/GetParams.py
@@ -0,0 +1,12 @@
+# переменные, который нужны для команды 1:
+# Узнать данные
+
+#FIXME: Подробно написать описание данного модуля
+'''
+GetParams.py
+
+Описание: ???
+'''
+PICK_NAME_SURNAME: str | None = None
+PICK_OBJECT: str | None = None
+NameSurnameList: list[str] = []
